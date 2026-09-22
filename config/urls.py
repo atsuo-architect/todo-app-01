a03254310e3b607923af649af10bdfc5e70d3653
atsuo-architect <249646496+atsuo-architect@users.noreply.github.com>
2026-09-23 08:06:53 +0900

    Implement authenticated Todo REST API

--- config/urls.py
+++ config/urls.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 from todos.views import TodoViewSet
 
@@ -12,4 +16,6 @@ router.register(r"todos", TodoViewSet, basename="todo")
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/", include(router.urls)),
+    path("api/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("api/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
 ]
