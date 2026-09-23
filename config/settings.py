070f128bb6079e3c61cf7e3889fd03adf5db86aa
atsuo-architect <249646496+atsuo-architect@users.noreply.github.com>
2026-09-23 18:12:02 +0900

    Add Django error logging

--- config/settings.py
+++ config/settings.py
@@ -137,3 +137,20 @@ SIMPLE_JWT = {
     "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
     "AUTH_HEADER_TYPES": ("Bearer",),
 }
+
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "handlers": {
+        "console": {
+            "class": "logging.StreamHandler",
+        },
+    },
+    "loggers": {
+        "django": {
+            "handlers": ["console"],
+            "level": "ERROR",
+            "propagate": False,
+        },
+    },
+}
